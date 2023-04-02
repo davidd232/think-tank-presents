ede8188eba240e8d3f09077865161c71f1f56f3f
David Darley <daviddar232@gmail.com>
2023-04-01 20:15:06 -0400

    adding a basic header with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { ListingSlider } from './components/ListingSlider/ListingSlider';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ListingSlider />
     </div>
   );
