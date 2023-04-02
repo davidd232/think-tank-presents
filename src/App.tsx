36e294bb37acf86c992e3787d9423b8cdd30f7de
David Darley <daviddar232@gmail.com>
2023-04-02 09:52:30 -0400

    adding sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import { Login } from './components/Login/Login';
 import { Main } from './components/Main/Main';
+import { SignUp } from './components/Signup/Signup';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
-      <Route path="/login" element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
